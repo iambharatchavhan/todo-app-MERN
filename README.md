5958c6d5edf446abc3c88dd7118d5ad28f8dc0dc
iambharatchavhan <bharatchavhan141@gmail.com>
2024-01-02 21:47:02 +0530

    feat:backend created for todo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple TO-DO app (fullstack)
+
+Features:
+ - User can add TODOs
+ - User can Read TODOs
+ - User can mark TODO as done 
+
+ 
---
+++ backend/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+
+// connect to database
+
+mongoose.connect("mongodb+srv://bharatchavhan141:MAQEZNN4mCMS8FQH@cluster0.wduzg.mongodb.net/")
+
+
+// create schemas
+const TodoSchema = mongoose.Schema({
+    title : String,
+    description: String,
+    completed: Boolean
+})
+
+const Todo = mongoose.model('Todo', TodoSchema)
+
+
+module.exports = {Todo}
