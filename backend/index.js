a4096fbda4ebbe14d9ced0d403812b4239a34237
iambharatchavhan <bharatchavhan141@gmail.com>
2024-01-05 10:43:07 +0530

    feat(index.js): delete end point added

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,21 @@
-
 const express = require("express");
 const { Todo } = require("./database");
-const { createTodo, updateTodo } = require("./types");  
-const cors = require('cors')
+const { createTodo, updateTodo, deleteTodo } = require("./types");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/demo",(req,res)=>{
-    res.send("Hello world")
-})
-
-
+app.get("/demo", (req, res) => {
+  res.send("Hello world");
+});
 
 app.post("/todo", async (req, res) => {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
-  
+
   if (!parsedPayload.success) {
     res.status(411).json({ message: "please provide valid inputs" });
     return;
@@ -28,16 +25,13 @@ app.post("/todo", async (req, res) => {
   await Todo.create({
     title: createPayload.title,
     description: createPayload.description,
-    completed: false
+    completed: false,
   });
 
   res.json({
     msg: "todo is created successfully",
   });
 });
-
-
-
 
 // get todos with find function
 app.get("/todos", async (req, res) => {
@@ -53,18 +47,30 @@ app.put("/completed", async (req, res) => {
     return;
   }
 
-// update in mongodb
-//  Whatever object has the _id please update completed as true
+  // update in mongodb
+  //  Whatever object has the _id please update completed as true
 
-  await Todo.updateOne(
-    { _id: req.body.id,},
-    { completed: true }
-  );
+  await Todo.updateOne({ _id: req.body.id }, { completed: true });
 
   res.json({
     msg: "bravo your task has been updated",
   });
 });
 
+app.delete("/deleted", async (req, res) => {
+  const deletePayload = req.body;
+  const parsedPayload = deleteTodo.safeParse(deletePayload);
 
+  if (!parsedPayload.success) {
+    res.status(404).json({ message: "Todo Is Not Available" });
+  }
+  
+  const deletedItem = await Todo.findOneAndDelete({ _id: req.body.id });
+
+  if (!deletedItem) {
+    res.status(404).json({ message: "Provide valid id" });
+  }
+
+  res.json({ message: "Todo Deleted Successfully" });
+});
 app.listen(port);
